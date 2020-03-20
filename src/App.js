314f14acc773ca163e4b065b142081989ef79282
Leroy Clarke Jr <lclarkejr94@gmail.com>
2020-03-20 15:36:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import loader from "./images/loader.svg";
+import clearButton from "./images/close-icon.svg";
+import Gif from "./Gif";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const randomChoice = arr => {
+  const randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+};
+
+const Header = ({ clearSearch, hasResults }) => (
+  <div className="header grid">
+    {hasResults ? (
+      <button onClick={clearSearch}>
+        <img src={clearButton} />
+      </button>
+    ) : (
+      <h1 className="title">Giphy API Search</h1>
+    )}
+  </div>
+);
+
+const UserHint = ({ loading, hintText }) => (
+  <div className="user-hint">
+    {loading ? <img className="block mx-auto" src={loader} /> : hintText}
+  </div>
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      hintText: "",
+      gifs: []
+    };
+  }
+
+  searchGiphy = async searchTerm => {
+    this.setState({
+      loading: true
+    });
+
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=v6NhNfqg3o1nQCcXRUBfBDfrxMcl9mv7&q=${searchTerm}&limit=25&offset=0&rating=G&lang=en`
+      );
+      const { data } = await response.json();
+      if (!data.length) {
+        throw `Nothing found for  ${searchTerm}`;
+      }
+
+      const randomGif = randomChoice(data);
+
+      console.log(randomGif);
+      console.log(data);
+
+      this.setState((prevState, props) => ({
+        ...prevState,
+        gifs: [...prevState.gifs, randomGif],
+        loading: false,
+        hintText: `Hit enter to see more ${searchTerm}`
+      }));
+    } catch (error) {
+      this.setState((prevState, props) => ({
+        ...prevState,
+        hintText: error,
+        loading: false
+      }));
+      console.log(error);
+    }
+  };
+
+  handleChange = event => {
+    const { value } = event.target;
+    //we tak eour old props and spread them out..
+    //and then we overwrite the ones we want after
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: value,
+      hintText: value.length > 2 ? `Hit enter to search ${value}` : ""
+    }));
+  };
+
+  handleKeyPress = event => {
+    const { value } = event.target;
+
+    if (value.length > 2 && event.key === "Enter") {
+      this.searchGiphy(value);
+    }
+    console.log(event.key);
+  };
+
+  clearSearch = () => {
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: "",
+      hintText: "",
+      gifs: []
+    }));
+    this.textInput.focus();
+  };
+
+  render() {
+    const { searchTerm, gifs } = this.state;
+    const hasResults = gifs.length;
+
+    return (
+      <div className="page">
+        <Header clearSearch={this.clearSearch} hasResults={hasResults} />
+
+        <div className="search grid">
+          {this.state.gifs.map(gif => (
+            <Gif {...gif} />
+          ))}
+
+          <input
+            className="input grid-item"
+            placeholder="Type Something"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            value={searchTerm}
+            ref={input => {
+              this.textInput = input;
+            }}
+          />
+        </div>
+        <UserHint {...this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
